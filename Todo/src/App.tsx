0e4311c4f3d3f028b47ad546ddd3af09da9ce0b4
Farseena-M <farseenamoyan19@gmail.com>
2024-11-02 16:23:26 +0530

    add dnd area

--- Todo/src/App.tsx
+++ Todo/src/App.tsx
@@ -3,7 +3,6 @@ import { Input } from './component/Input'
 import { Button } from './component/Button'
 import { ItemList } from './component/ItemList'
 import { useTodoApi } from './component/api/api'
-import { DndContext } from '@dnd-kit/core';
 
 function App() {
 
@@ -31,32 +30,30 @@ function App() {
 
 
   return (
-    <DndContext >
-      <div className='h-[100vh] flex flex-col items-center mt-5'>
-        <h1 className='text-3xl font-bold mb-8 text-gray-600 font-serif'>
-          TaskTracker
-        </h1>
-        <div className='flex'>
-          <div className='flex flex-col items-start mr-10'>
-            <div className='w-[400px]'>
-              <form onSubmit={handleSubmit} className='mb-5'>
-                <Input inputValue={inputValue} setInputValue={setInputValue} type='text' />
-                <Button className='bg-gray-700 text-white w-full p-2'>Add</Button>
-              </form>
-            </div>
-            <div className='w-[400px] h-52 overflow-y-auto'>
-              <ItemList items={items} handleEdit={handleEdit} handleDelete={deleteTodo} />
-            </div>
+    <div className='h-[100vh] flex flex-col items-center mt-5'>
+      <h1 className='text-3xl font-bold mb-8 text-gray-600 font-serif'>
+        TaskTracker
+      </h1>
+      <div className='flex'>
+        <div className='flex flex-col items-start mr-10'>
+          <div className='w-[400px]'>
+            <form onSubmit={handleSubmit} className='mb-5'>
+              <Input inputValue={inputValue} setInputValue={setInputValue} type='text' />
+              <Button className='bg-gray-700 text-white w-full p-2'>Add</Button>
+            </form>
           </div>
-          <div className='flex flex-col items-start'>
-            <div className='w-[400px] h-52 bg-gray-700 border border-gray-400 rounded-lg p-4'>
-              <h2 className='text-xl font-bold mb-4 text-gray-300 text-center font-serif'>Completed Tasks</h2>
-              <div className='text-gray-500'></div>
-            </div>
+          <div className='w-[400px] h-52 overflow-y-auto'>
+            <ItemList items={items} handleEdit={handleEdit} handleDelete={deleteTodo} />
+          </div>
+        </div>
+        <div className='flex flex-col items-start'>
+          <div className='w-[400px] h-52 bg-gray-700 border border-gray-400 rounded-lg p-4'>
+            <h2 className='text-xl font-bold mb-4 text-gray-300 text-center font-serif'>Completed Tasks</h2>
+            <div className='text-gray-500'></div>
           </div>
         </div>
       </div>
-    </DndContext>
+    </div>
   )
 }
 
